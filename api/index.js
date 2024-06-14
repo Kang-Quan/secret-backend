742717a94d7199800df20da101a404fb7bffed47
Kang-Quan <KangQuan_lee@yahoo.com.sg>
2024-06-14 10:39:27 +0800

    Add api to send tele message from FE to BE

--- api/index.js
+++ api/index.js
@@ -1,8 +1,28 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
+const TelegramBot = require('node-telegram-bot-api');
+const port = process.env.PORT || 8000;
+const token = process.env.TELEBOTKEY || "";
+const bot = new TelegramBot(token, { polling: false });
+const chatID = process.env.CHATID;
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+    // Sending a message via the bot
+    if (bot && bot.sendMessage && chatID) {
+      bot.sendMessage(chatID, 'Received your message')
+        .then(() => {
+          res.send("Express on Vercel");
+        })
+        .catch((error) => {
+          console.error("Error sending bot message:", error);
+          res.status(500).send("Error sending bot message");
+        });
+    } else {
+      res.send("Express on Vercel");
+    }
+  });
 
-app.listen(8000, () => console.log("Server ready on port 3000."));
+app.listen(port, () => console.log("Server ready on port " + port));
 
 module.exports = app;
